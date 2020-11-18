81da3fc06c24478417c815fe287b64d4730edd2a
yuta ohira <alesion90@gmail.com>
2020-11-18 18:36:52 +0900

    Node側からLEDを制御できるようにした

--- awecon.ino
+++ awecon.ino
@@ -1,16 +1,38 @@
-#define LED_PIN1 10
+#define LED_PIN1 11
+#define LED_PIN2 10
 
 void setup()
 {
-    pinMode(LED_PIN1, OUTPUT);
+    // setup
     Serial.begin(9600);
+    pinMode(LED_PIN1, OUTPUT);
+    pinMode(LED_PIN2, OUTPUT);
+
+    digitalWrite(LED_PIN1, HIGH);
+    digitalWrite(LED_PIN2, LOW);
 }
 
 void loop()
 {
-    Serial.println(200);
-    digitalWrite(LED_PIN1, HIGH);
+    if (Serial.available() > 0) // Read from serial port
+    {
+        char ReaderFromNode; // Store current character
+        ReaderFromNode = (char)Serial.read();
+        convertToState(ReaderFromNode); // Convert character to state
+    }
     delay(1000);
-    digitalWrite(LED_PIN1, LOW);
-    delay(1000);
+}
+
+void convertToState(char chr)
+{
+    if (chr == 'w')
+    {
+        Serial.println("ON");
+        digitalWrite(LED_PIN2, HIGH);
+    }
+    if (chr == 't')
+    {
+        Serial.println("OFF");
+        digitalWrite(LED_PIN2, LOW);
+    }
 }
