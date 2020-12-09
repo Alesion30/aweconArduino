086db8d724669645b17814191a4b25fc72e74a5d
yuto <tanaka.yuto.3002@gmail.com>
2020-12-09 12:07:43 +0900

    エアコン起動関数を追加

--- awecon.ino
+++ awecon.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+#include <PanasonicHeatpumpIR.h>
 #include "Wire.h"
 #define TMP102_I2C_ADDRESS 0x48
 #define LED_PIN1 11
@@ -17,14 +19,25 @@ void setup()
 
 void loop()
 {
-    if (Serial.available() > 0)
-    {
-        char ReaderFromNode;
-        ReaderFromNode = (char)Serial.read();
-        convertToState(ReaderFromNode);
-    }
-    Serial.println(get_temp());
-    delay(1000);
+    IRSenderPWM irSender(3);
+    PanasonicDKEHeatpumpIR *heatpumpIR;
+
+    heatpumpIR = new PanasonicDKEHeatpumpIR();
+    heatpumpIR->send(irSender, POWER_ON, MODE_HEAT, FAN_AUTO, 24, VDIR_AUTO, HDIR_AUTO);
+    delay(5000);
+
+    // if (Serial.available() > 0)
+    // {
+    //     char ReaderFromNode;
+    //     ReaderFromNode = (char)Serial.read();
+    //     convertToState(ReaderFromNode);
+    // }
+    // Serial.println(get_temp());
+    // digitalWrite(LED_PIN2, HIGH);
+    // delay(1000);
+
+    // digitalWrite(LED_PIN2, LOW);
+    // delay(1000);
 }
 
 // Arduinoの制御 NodeServerからactionが送られてくる
